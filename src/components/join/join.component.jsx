60319a4830b5fe85f3469b22199b5ca462c34c84
Ross Gartland <rgartland117@hotmail.com>
2020-10-02 18:34:35 +0100

    fixed minor css bug with nav

--- src/components/join/join.component.jsx
+++ src/components/join/join.component.jsx
@@ -6,10 +6,10 @@ const Join = () => {
         <div id="join" className="join-container my-container">
             <h1 className="display-3">Join</h1>
             <p className="text-center">To join our society, click on the following link you must be a student at Ulster University.</p>
-            <a href="https://www.uusu.org/organisation/21214/" target="_blank" rel="noopener noreferrer">https://www.uusu.org/organisation/21214/</a>
+            <a className="sub_a" href="https://www.uusu.org/organisation/21214/" target="_blank" rel="noopener noreferrer">https://www.uusu.org/organisation/21214/</a>
             <p>To find out if our society is right for you, give us an email and we would be happy to discuss this further.</p>
             <p>If you are an employer interested in giving a talk at our society, please send an email to the following address.</p>
-            <a href="mailto:futuresocietyuu@gmail.com">futuresocietyuu@gmail.com</a>
+            <a className="sub_a" href="mailto:futuresocietyuu@gmail.com">futuresocietyuu@gmail.com</a>
         </div>
     )
 }
